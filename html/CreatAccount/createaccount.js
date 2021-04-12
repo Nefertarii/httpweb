b4fe4fc9c603abe43fa180bd2571842a1ba4caa4
Nefertarii <47806751+Nefertarii@users.noreply.github.com>
2021-04-12 20:59:47 +0800

    Add files via upload

--- html/CreatAccount/createaccount.js
+++ html/CreatAccount/createaccount.js
@@ -1,87 +1,91 @@
 	// function name   AaaBbbCcc
 	// variable name   aaaBbbCcc
 	
-	var passwdLabel = document.getElementById("passwd_label");
-	var passwdInput = document.getElementById("passwd_input");
-	var userNameInput = document.getElementById("username_input");
-	var userNameError = document.getElementById("username_error");
-	var emailInput = document.getElementById("email_input");
-	var emailError = document.getElementById("email_error");
-	passwdInput.addEventListener("keydown", PasswdDetect);
-	userNameInput.addEventListener("keydown", UserNameDetect);
-	emailInput.addEventListener("keydown", EmailDetect);
+
+	var nameInput = document.getElementsByClassName("input_name");
+	var nameLabel = document.getElementById("input_error_name");
+	var emailInput = document.getElementsByClassName("input_email");
+	var emailLabel = document.getElementById("input_error_email");
+	var passwdInput = document.getElementsByClassName("input_passwd");
+	var passwdLabel = document.getElementById("input_error_passwd");
 	
-	function UserNameDetect()
+	
+	nameInput[0].addEventListener("keydown", NameDetect);
+	emailInput[0].addEventListener("keydown", EmailDetect);
+	passwdInput[0].addEventListener("keydown", PasswdDetect);
+	
+	function NameDetect()
 	{
-		emailError.style.visibility="hidden";
-		userNameInput.style.backgroundImage = "url('../Image/gif/delay-16px.gif')";
-		setTimeout(UserNameInput, 500);
-		function UserNameInput() {
-			if (8 <= userNameInput.value.length) {
-				userNameError.style.visibility="visible";
-				userNameError.className = "success"
-				userNameError.innerHTML = userNameError.innerHTML=
-				"Username " + userNameInput.value + " is a available";
-				userNameInput.style.borderColor = "green";
-				userNameInput.style.backgroundImage = "url('../Image/png/success.png')";
+		nameLabel.style.visibility="hidden";
+		nameInput[0].style.backgroundImage = "url('../Image/gif/delay-16px.gif')";
+		setTimeout(inline, 500);
+		function inline() {
+			if (8 <= nameInput[0].value.length) {
+				nameLabel.style.visibility = "visible";
+				nameLabel.innerHTML = "用户名 " + nameInput[0].value + " 可以使用";
+				nameLabel.className = "input_success";
+				nameInput[0].style.borderColor = "#34d058";
+				nameInput[0].style.backgroundImage = "url('../Image/png/success.png')";
 			} 
-			else if (userNameInput.value.length == 0) {
-				userNameError.style.visibility="hidden";
-				userNameInput.style.backgroundImage = "url('none')";
-				userNameInput.style.borderColor = "#d8dee2";
+			else if (nameInput[0].value.length == 0) {
+				nameLabel.style.visibility = "hidden";
+				nameInput[0].style.backgroundImage = "url('none')";
+				nameInput[0].style.borderColor = "#d8dee2";
 			}
 			else {
-				userNameError.style.visibility="visible";
-				userNameError.className = "error"
-				userNameError.innerHTML = userNameError.innerHTML=
-				"Username " + userNameInput.value + " is not a available";
-				userNameInput.style.borderColor = "red";
-				userNameInput.style.backgroundImage = "url('../Image/png/error.png')";
+				nameLabel.style.visibility="visible";
+				nameLabel.innerHTML = "用户名 " + nameInput[0].value + " 不合法或已被使用";
+				nameLabel.className = "input_error";
+				nameInput[0].style.borderColor = "#F97583";
+				nameInput[0].style.backgroundImage = "url('../Image/png/error.png')";
 			}
 		}
 	}
 	function EmailDetect()
 	{
-		emailError.style.visibility="hidden";
-		emailInput.style.backgroundImage = "url('../Image/gif/delay-16px.gif')";
-		setTimeout(EmailInput, 500);
+		emailLabel.style.visibility="hidden";
+		emailInput[0].style.backgroundImage = "url('../Image/gif/delay-16px.gif')";
+		setTimeout(inline, 500);
 		var STATE="none";
 		var pattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-		function EmailInput() {
-			if (true == pattern.test(emailInput.value)) {
-				emailError.style.visibility="hidden";
-				emailError.className = "success"
-				emailInput.style.borderColor = "green";
-				emailInput.style.backgroundImage = "url('../Image/png/success.png')";
+		function inline() {
+			if (true == pattern.test(emailInput[0].value)) {
+				emailLabel.style.visibility="hidden";
+				emailLabel.className = "input_success"
+				emailInput[0].style.borderColor = "#34d058";
+				emailInput[0].style.backgroundImage = "url('../Image/png/success.png')";
 			} 
-			else if (0 == emailError.value) {
-				emailError.style.visibility="hidden";
-				emailInput.style.backgroundImage = "url('none')";
-				emailInput.style.borderColor = "#d8dee2";
+			else if (0 == emailLabel.value) {
+				emailLabel.style.visibility="hidden";
+				emailInput[0].style.backgroundImage = "url('none')";
+				emailInput[0].style.borderColor = "#d8dee2";
 			}
 			else {
-				emailError.style.visibility="visible";
-				emailError.className = "error"
-				emailError.innerHTML = emailError.innerHTML=
-				"Email is invalid or already taken"
-				emailInput.style.borderColor = "red";
-				emailInput.style.backgroundImage = "url('../Image/png/error.png')";
+				emailLabel.style.visibility="visible";
+				emailLabel.className = "input_error";
+				emailLabel.innerHTML ="邮箱不合法或已被使用";
+				emailInput[0].style.borderColor = "#F97583";
+				emailInput[0].style.backgroundImage = "url('../Image/png/error.png')";
 			}
 		}
 	}
-	
 	function PasswdDetect() {
-		passwdInput.style.backgroundImage = "url('../Image/delay-16px.gif')";
+		passwdInput[0].style.backgroundImage = "url('../Image/delay-16px.gif')";
 		setTimeout(PassWordInput, 500);
 		function PassWordInput() {
-			if (8 <= passwdInput.value.length) {
-				passwdLabel.style.color = "green";
-				passwdInput.style.borderColor = "green";
-				passwdInput.style.backgroundImage = "url('../Image/png/success.png')";
-			} else {
-				passwdLabel.style.color = "red";
-				passwdInput.style.borderColor = "red";
-				passwdInput.style.backgroundImage = "url('../Image/png/error.png')";
+			if (8 <= passwdInput[0].value.length) {
+				passwdLabel.style.color = "#34d058";
+				passwdInput[0].style.borderColor = "#34d058";
+				passwdInput[0].style.backgroundImage = "url('../Image/png/success.png')";
+			} 
+			else if (passwdInput[0].value.length == 0) {
+				passwdInput[0].style.backgroundImage = "url('none')";
+				passwdInput[0].style.borderColor = "#d8dee2";
+			}
+			else {
+				passwdLabel.style.color = "#86181D";
+				passwdInput[0].style.borderColor = "#F97583";
+				passwdInput[0].style.backgroundImage = "url('../Image/png/error.png')";
 			}
 		}
 	}
