341fca95082ecb00e929fe9fc025aa00b3196c70
Nefertarii <47806751+Nefertarii@users.noreply.github.com>
2021-05-20 21:28:46 +0800

    前端进行处理了json解析并使用

--- html/index.js
+++ html/index.js
@@ -2,26 +2,29 @@ var login = new XMLHttpRequest();
 var login_name = document.getElementById("username").value;
 var login_passwd = document.getElementById("password").value;
 var login_button = document.getElementById("login_button");
-login_button.onclick = function() 
-{
+
+var avatar = document.getElementById("avatar_success");
+var left_info_fail = document.getElementById("left_info_fail");
+var left_info_success = document.getElementById("left_info_success");
+var left_info_success_username = document.getElementById("left_info_success_username");
+
+login_button.onclick = function() {
 	let login_name = document.getElementById("username").value;
 	let login_passwd = document.getElementById("password").value;
 	var tmp = login_name + "&" + login_passwd;
 	login.open("POST", "login", true);
 	login.send(tmp);
 }
-
-
-function state_Change() 
-{
-	if (login.readyState == 4) 
-	{ // 4 = "loaded"
-		if (login.status == 200) 
-		{ // 200 = "OK"
-			document.getElementById('p1').innerHTML = login.getAllResponseHeaders();
-		} else 
-		{
-			alert("Problem retrieving data:" + login.statusText);
+login.onreadystatechange = function() {
+	if (login.readyState == 4 && login.status == 200) {
+		var returnObj = eval("(" + login.responseText + ")");
+		if (returnObj.session == "success") {
+			left_info_fail.style.display = "none";
+			left_info_success.style.display = "block";
+			var image = returnObj.image;
+			var username = returnObj.username
+			avatar.style.backgroundImage = "url(" + image + ")";
+			left_info_success_username.innerHTML = username;
 		}
 	}
-}
+};
